ddd6be4a86a9dcdd7280545ba0a94f4233adfe0c
aeuna <4294psy@naver.com>
2020-08-23 01:18:12 +0900

    Use context API

--- begin_react/src/CreateUser.js
+++ begin_react/src/CreateUser.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { UserDispatch } from './App';
+import useInputs from './hooks/ useInputs';
 
-const CreateUser = ({ username, email, onChange, onCreate }) => {
-  console.log('입력창');
+const CreateUser = () => {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
+
+  const nextId = useRef(4);
+  const dispatch = useContext(UserDispatch);
+
+  const onCreate = () => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
+    reset();
+    nextId.current += 1;
+  };
+
   return (
     <div>
       <input
